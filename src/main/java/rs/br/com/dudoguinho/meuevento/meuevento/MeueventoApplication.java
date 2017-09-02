718f9882313da5891b68fbfcdb8cbd6183ede9bb
GIAN REGIS FRICK <ALU201731119@uniritter.local>
2017-09-01 21:09:36 -0300

    rick eh quem manda

---
+++ src/main/java/rs/br/com/dudoguinho/meuevento/meuevento/MeueventoApplication.java
@@ -0,0 +1,12 @@
+package rs.br.com.dudoguinho.meuevento.meuevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeueventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeueventoApplication.class, args);
+	}
+}
