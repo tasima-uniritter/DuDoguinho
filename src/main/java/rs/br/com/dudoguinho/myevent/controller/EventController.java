b68251306dcd53202ebbe267336e0d37271594e0
GIAN REGIS FRICK <ALU201731119@uniritter.local>
2017-09-02 11:36:56 -0300

    versao com log

--- src/main/java/rs/br/com/dudoguinho/myevent/controller/EventController.java
+++ src/main/java/rs/br/com/dudoguinho/myevent/controller/EventController.java
@@ -2,9 +2,12 @@ package rs.br.com.dudoguinho.myevent.controller;
 
 import java.time.LocalDate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +16,25 @@ import rs.br.com.dudoguinho.myevent.dto.EventDto;
 @RestController("/event")
 public class EventController {
 	
-	  @GetMapping(value="/{nome}")
-	  public ResponseEntity<EventDto> getEventoByNome(@RequestParam("nome") String nome) {
-		  EventDto dto = new EventDto();
-		  dto.setNome("Rick é demais2");
-		  dto.setData(LocalDate.now());
-		  return new ResponseEntity<EventDto>(dto, HttpStatus.OK);
-	  }
+	private static Logger log = LoggerFactory.getLogger(EventController.class);
+	
+	@RequestMapping("/")
+	public String home() {
+		log.info("Event Controller");
+	    return "Aqui !";
+	}
+	 
+	 @GetMapping(value="/{nome}")
+	 public ResponseEntity<EventDto> getEventoByNome(@RequestParam("nome") String nome) {
+		 EventDto dto = new EventDto();
+		 dto.setNome("Rick é demais2");
+		 dto.setData(LocalDate.now());
+		 return new ResponseEntity<EventDto>(dto, HttpStatus.OK);
+	 }
 
-	  public void updateEventoByNome(){}
+	 public void updateEventoByNome(){}
 	  
-	  public void saveEvento(){}
-	  
-	  public void deleteEvento(){}
+	 public void saveEvento(){}
+	 
+	 public void deleteEvento(){}
 }
