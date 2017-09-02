e0353118bee8309a082d8d0e33103bdbb7106296
GIAN REGIS FRICK <ALU201731119@uniritter.local>
2017-09-02 11:55:45 -0300

    versao com bd

--- src/main/java/rs/br/com/dudoguinho/myevent/controller/EventController.java
+++ src/main/java/rs/br/com/dudoguinho/myevent/controller/EventController.java
@@ -7,8 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import rs.br.com.dudoguinho.myevent.dto.EventDto;
@@ -18,14 +17,14 @@ public class EventController {
 	
 	private static Logger log = LoggerFactory.getLogger(EventController.class);
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		log.info("Event Controller");
 	    return "Aqui !";
 	}
 	 
-	@GetMapping(value="/{nome}")
-	public ResponseEntity<EventDto> getEventoByNome(@RequestParam("nome") String nome) {
+	@GetMapping("/{nome}")
+	public ResponseEntity<EventDto> getEventoByNome(@PathVariable	("nome") String nome) {
 		EventDto dto = new EventDto();
 		dto.setNome("Rick é demais2");
 		dto.setData(LocalDate.now());
--- src/main/java/rs/br/com/dudoguinho/myevent/model/Event.java
+++ src/main/java/rs/br/com/dudoguinho/myevent/model/Event.java
@@ -2,11 +2,14 @@ package rs.br.com.dudoguinho.myevent.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+@Entity
 @EqualsAndHashCode @ToString
 @Getter @Setter
 public class Event {
