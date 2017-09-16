bbc5bd8602c5bf02a983ae54e256d03e53100a9d
GIAN REGIS FRICK <ALU201731119@uniritter.local>
2017-09-16 09:52:37 -0300

    ajustando controller

--- src/main/java/rs/br/com/dudoguinho/myevent/controller/EventController.java
+++ src/main/java/rs/br/com/dudoguinho/myevent/controller/EventController.java
@@ -5,26 +5,28 @@ import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import rs.br.com.dudoguinho.myevent.dto.EventDto;
 
-@RestController("/event")
+@RestController
 public class EventController {
 	
 	private static Logger log = LoggerFactory.getLogger(EventController.class);
 	
-	@GetMapping("/")
+	@RequestMapping(value = "/event/", method = RequestMethod.GET)
 	public String home() {
 		log.info("Event Controller");
 	    return "Aqui !";
 	}
 	 
-	@GetMapping("/{nome}")
-	public ResponseEntity<EventDto> getEventoByNome(@PathVariable	("nome") String nome) {
+	@RequestMapping(value = "/event/{nome}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<EventDto> getEventoByNome(@PathVariable ("nome") String nome) {
 		EventDto dto = new EventDto();
 		dto.setName("Rick é demais2");
 		dto.setDate(LocalDate.now());
--- src/main/java/rs/br/com/dudoguinho/myevent/controller/TicketController.java
+++ src/main/java/rs/br/com/dudoguinho/myevent/controller/TicketController.java
@@ -2,14 +2,15 @@ package rs.br.com.dudoguinho.myevent.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/ticket")
+@RestController
 public class TicketController {
 	private static Logger log = LoggerFactory.getLogger(TicketController.class);
 	
-	@GetMapping("/")
+	@RequestMapping(value = "/ticket/", method = RequestMethod.GET)
 	public String home() {
 		log.info("Ticket Controller");
 	    return "Aqui !";
