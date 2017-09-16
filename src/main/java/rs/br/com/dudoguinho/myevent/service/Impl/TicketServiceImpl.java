aebfb3cbac13df6af4c8e444b546b00aff2ba509
GIAN REGIS FRICK <ALU201731119@uniritter.local>
2017-09-16 10:14:32 -0300

    services access dao

--- src/main/java/rs/br/com/dudoguinho/myevent/service/Impl/TicketServiceImpl.java
+++ src/main/java/rs/br/com/dudoguinho/myevent/service/Impl/TicketServiceImpl.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import rs.br.com.dudoguinho.myevent.controller.EventController;
+import rs.br.com.dudoguinho.myevent.dao.impl.TicketDaoImpl;
 import rs.br.com.dudoguinho.myevent.service.TicketService;
 import rs.br.com.dudoguinho.myevent.validator.TicketValidator;
 
@@ -14,6 +14,7 @@ public class TicketServiceImpl implements TicketService {
 	private static Logger log = LoggerFactory.getLogger(TicketServiceImpl.class);
 	
 	@Autowired private TicketValidator validator;
+	@Autowired private TicketDaoImpl repository;
 
 	@Override
 	public void createTicket() {
